f8f37931f1e44a210f185bcacd4b91e3bb091014
Carlos Pereira <carlospereira@MacBook-Pro-de-Carlos.local>
2021-06-26 19:27:57 -0300

    Implement share feature

--- src/components/Form/ResultIMC/ResultIMC.js
+++ src/components/Form/ResultIMC/ResultIMC.js
@@ -1,12 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Share } from 'react-native'
 
 //styles
 import styles from './style'
 
 const ResultIMC = ({ messageResultIMC, resultIMC }) => {
+
+    const onShare = async () => {
+        const result = await Share.share({
+            message: `Meu IMC é ${resultIMC}`
+        })
+    }
+
     return (
         <View style={styles.resultIMC}>
+            <View style={styles.boxShareButton}>
+                {resultIMC != null ?
+                    <TouchableOpacity style={styles.shareButton} onPress={onShare}>
+                        <Text style={styles.shareText}>
+                            Compartilhe
+                        </Text>
+                    </TouchableOpacity>
+                    :
+                    <View />
+                }
+            </View>
             <Text style={styles.information}>{messageResultIMC}</Text>
             <Text style={styles.numberIMC}>{resultIMC}</Text>
         </View>
--- src/components/Form/ResultIMC/style.js
+++ src/components/Form/ResultIMC/style.js
@@ -20,6 +20,25 @@ const styles = StyleSheet.create({
         fontSize: 48,
         color: "#ff0043",
         fontWeight: "bold",
+    },
+
+    boxShareButton: {
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+
+    shareButton: {
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+
+    shareText: {
+        color: "#fff",
+        fontWeight: "bold",
+        paddingHorizontal: 30,
     }
 })
 
